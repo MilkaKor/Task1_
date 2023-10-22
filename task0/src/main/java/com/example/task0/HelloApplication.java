174005bda82bd054d57c47796cec8c5ae77d9fde
MilkaKor <korchanovamila29@outlook.com>
2023-10-23 00:59:15 +0300

    Add task0_

--- task0/src/main/java/com/example/task0/HelloApplication.java
+++ task0/src/main/java/com/example/task0/HelloApplication.java
@@ -41,8 +41,6 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
-
     public static void main(String[] args) {
         launch();
     }
--- task0/src/main/java/com/example/task0/Procent.java
+++ task0/src/main/java/com/example/task0/Procent.java
@@ -1,26 +1,36 @@
 package com.example.task0;
 
 public class Procent {
-    Procent(){
+    Procent() {
         sum = 0;
     }
-    Procent(float value){sum = value;}
+
+    Procent(float value) {
+        sum = value;
+    }
+
     private float sum;
-    public void setSum(float value){this.sum = value;}
-    public float getSum( ){return this.sum;}
+
+    public void setSum(float value) {
+        this.sum = value;
+    }
+
+    public float getSum() {
+        return this.sum;
+    }
+
     private double pr;
-    public float countPr(int pr)
-    {
 
-        return (pr*this.sum)/100;
+    public float countPr(int pr) {
+
+        return (pr * this.sum) / 100;
     }
 
-    public float countSum(float sum, float pr)
-    {
-        return sum + sum*pr/100;
+    public float countSum(float sum, float pr) {
+        return sum + sum * pr / 100;
     }
-    public  int countSumrnd(int pr)
-    {
-        return Math.round(this.sum+this.sum*pr/100);
+
+    public int countSumrnd(int pr) {
+        return Math.round(this.sum + this.sum * pr / 100);
     }
 }
